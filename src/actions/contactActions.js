abdb2ea2f999bde32140a3c7981fbfd8ecef8256
Graham Cottridge <codinggc@gmail.com>
2019-04-21 23:44:46 +0100

    Add post request

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -10,16 +10,27 @@ export const getContacts = () => async dispatch => {
 };
 
 export const deleteContact = id => async dispatch => {
-  await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
-  dispatch({
-    type: DELETE_CONTACT,
-    payload: id
-  });
+  try {
+    await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+    dispatch({
+      type: DELETE_CONTACT,
+      payload: id
+    });
+  } catch (e) {
+    dispatch({
+      type: DELETE_CONTACT,
+      payload: id
+    });
+  }
 };
 
-export const addContact = contact => {
-  return {
+export const addContact = contact => async dispatch => {
+  const res = await axios.post(
+    "https://jsonplaceholder.typicode.com/users",
+    contact
+  );
+  dispatch({
     type: ADD_CONTACT,
-    payload: contact
-  };
+    payload: res.data
+  });
 };
